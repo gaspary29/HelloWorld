a64d996a1287e51dfb10a58639c1136af98d3317
gaspary29 <www.macka@mail.ru>
2024-02-07 23:23:12 +0300

    работаа над ошибками по заданию 2.4, задача 5

--- hw-4.js
+++ hw-4.js
@@ -20,11 +20,11 @@ for (item in obj)
     console.log(`${item} зарплата ${obj[item]} долларов`)
 
 let num = 0
-
-for (let c = 1000; c > 50; c /= 2) {
-    num++;
-    console.log(`Получилось число:${c}`)
-};
+let c = 1000
+while (c > 50) 
+{c /= 2; num++;  console.log(`Получилось число:${c}`)
+    
+} 
 
 console.log(`Всего итераций:${num}`);
 
